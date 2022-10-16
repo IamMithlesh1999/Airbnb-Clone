da5c03a0c9b0c01b41c8c73d75f3b44f9942db59
Mithlesh Kumar <mk4391911@gmail.com>
2022-10-16 13:34:36 +0530

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css'
+
+function Footer() {
+    return (
+
+        <div className="footer">
+        <p>© 2022 Airbnb clone! No right reserved - this is a demo !!</p>  
+        <p>by Mithlesh Kumar</p>   
+        </div>
+    )
+}
+
+export default Footer
